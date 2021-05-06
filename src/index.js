59f635f038add0b27509e6ac9e17c88ad5cfa81a
Rogerio Pavan <rogerio_pavan@hotmail.com>
2021-05-06 09:37:10 -0300

    refactor: remove optional chaining

--- src/index.js
+++ src/index.js
@@ -10,23 +10,23 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  const [found] = users.filter((user)=>  user.username === request.headers?.username)
-  
-  if(!found){
+  const [found] = users.filter((user) => user.username === request.headers.username)
+
+  if (!found) {
     return response.status(400).json({
       error: 'Invalid user'
     })
   }
- 
+
   request.user = found
   return next()
 }
 
 app.post('/users', (request, response) => {
   const { name, username } = request.body
-  const [found] = users.filter((user)=>  user.username === username && user.name === name)
-  
-  if(found){
+  const [found] = users.filter((user) => user.username === username && user.name === name)
+
+  if (found) {
     return response.status(400).json({
       error: 'User already registered'
     })
@@ -34,8 +34,8 @@ app.post('/users', (request, response) => {
 
   const user = {
     id: uuidv4(),
-    name, 
-    username,  
+    name,
+    username,
     todos: []
   }
 
@@ -54,11 +54,11 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
   const todo = {
     id: uuidv4(),
     title,
-    done: false, 
-    deadline: new Date(deadline), 
+    done: false,
+    deadline: new Date(deadline),
     created_at: new Date()
   }
-  
+
   const userIndex = users.findIndex(user => user.id = request.user.id)
   users[userIndex].todos.push(todo)
   return response.status(201).json(todo)
@@ -67,19 +67,19 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   const userIndex = users.findIndex(user => user.id === request.user.id)
   const todoIndex = users[userIndex].todos.findIndex(todo => todo.id === request.params.id)
-  const oldTodo =  users[userIndex].todos[todoIndex]
+  const oldTodo = users[userIndex].todos[todoIndex]
 
-  if(!oldTodo) {
+  if (!oldTodo) {
     return response.status(404).json({
       error: 'Todo not found'
     })
   }
- 
+
   const newTodo = {
     ...oldTodo,
     ...request.body
   }
-  
+
   users[userIndex].todos[todoIndex] = newTodo
   return response.status(200).json(newTodo)
 });
@@ -87,33 +87,33 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
   const userIndex = users.findIndex(user => user.id === request.user.id)
   const todoIndex = users[userIndex].todos.findIndex(todo => todo.id === request.params.id)
-  const oldTodo =  users[userIndex].todos[todoIndex]
+  const oldTodo = users[userIndex].todos[todoIndex]
 
-  if(!oldTodo) {
+  if (!oldTodo) {
     return response.status(404).json({
       error: 'Todo not found'
     })
   }
- 
+
   const newTodo = {
     ...oldTodo,
     done: true
   }
-  
+
   users[userIndex].todos[todoIndex] = newTodo
   return response.status(200).json(newTodo)
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   const userIndex = users.findIndex(user => user.id === request.user.id)
-  const todoIndex = users[userIndex].todos?.findIndex(todo => todo.id === request.params.id)
+  const todoIndex = users[userIndex].todos.findIndex(todo => todo.id === request.params.id)
 
-  if(todoIndex === -1) {
+  if (todoIndex === -1) {
     return response.status(404).json({
       error: 'Todo not found'
     })
   }
- 
+
   const newTodos = users[userIndex].todos.filter(todo => todo.id !== request.params.id)
   users[userIndex].todos = newTodos
   return response.status(204).json()
